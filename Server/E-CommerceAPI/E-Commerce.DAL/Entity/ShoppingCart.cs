3e331b4b6df64612589b40f1eafabb94ed2ec0e5
zyadamin <66642836+zyadamin@users.noreply.github.com>
2024-02-13 02:48:13 +0200

    Add shopping cart

---
+++ Server/E-CommerceAPI/E-Commerce.DAL/Entity/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.DAL.Entity
+{
+    public class ShoppingCart
+    {
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+         
+       public ICollection<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+
+    }
+}
